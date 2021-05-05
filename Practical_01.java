4753de9a2a6a80bbbe5eeb99d12055ab16d2d117
Rajvi <76648851+Rajviii@users.noreply.github.com>
2021-05-05 12:19:21 +0530

    Add files via upload
    Hello JAVA!!

---
+++ Practical_01.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Practical_01 {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Welcome to Java, Learning Java Now and Programming is fun");
+    }
+}
